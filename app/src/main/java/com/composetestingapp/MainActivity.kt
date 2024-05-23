904a8c94390b380abdc1127342e131cc1e649efd
Magister <xqjh@mail.ru>
2024-05-23 18:06:09 +0300

    MainActivity работает на layOut Внутри добавлен viewPager с листом из двух фрагментов. Тела фрагментов задаются с помощью compose а не layout

--- app/src/main/java/com/composetestingapp/MainActivity.kt
+++ app/src/main/java/com/composetestingapp/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.composetestingapp
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.composetestingapp.databinding.MainLayoutBinding
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: MainLayoutBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContent {  }
+        binding = MainLayoutBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        val fragmentList:ArrayList<Fragment> = arrayListOf()
+        fragmentList.add(StartFragment())
+        fragmentList.add(SettingsFragment())
+
+
+        val demoCollectionAdapter = DemoCollectionAdapter(this,fragmentList)
+
+        binding.fragmentPager.adapter = demoCollectionAdapter
+
+
     }
 
+
+    class DemoCollectionAdapter(fragment: FragmentActivity, private val arrayFragment: ArrayList<Fragment>) :
+        FragmentStateAdapter(fragment) {
+        override fun getItemCount(): Int = arrayFragment.size
+        override fun createFragment(position: Int): Fragment = arrayFragment[position]
+    }
 }
---
+++ app/src/main/java/com/composetestingapp/SettingsFragment.kt
@@ -0,0 +1,23 @@
+package com.composetestingapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material3.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+
+class SettingsFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply { 
+            setContent { 
+                Text(text = "Settings")
+            }
+        }
+    }
+}
