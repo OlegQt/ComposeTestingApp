55c058739c2413ad9f6b944f3894809876ab4e33
OlegWork <xqjh@mail.ru>
2024-05-24 13:15:14 +0300

    Пробный UI для ввода текста

--- app/src/main/java/com/composetestingapp/presentation/fragment/ComposeFoo.kt
+++ app/src/main/java/com/composetestingapp/presentation/fragment/ComposeFoo.kt
@@ -1,11 +1,22 @@
 package com.composetestingapp.presentation.fragment
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.flow.MutableStateFlow
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -19,4 +30,29 @@ fun UserInputText(textLine: MutableStateFlow<String>) {
         },
         colors = TextFieldDefaults.textFieldColors(textColor = Color.Magenta)
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun UiTextFieldPreview() {
+    Column {
+        UiUserInput()
+        UiUserInput()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UiUserInput() {
+    var txt by remember { mutableStateOf("Text") }
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.Gray)
+    ) {
+        TextField(
+            modifier = Modifier.padding(16.dp),
+            value = txt,
+            onValueChange = { txt = it }
+        )
+    }
 }
