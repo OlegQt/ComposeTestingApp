8b5f5dd46329a2d3ba497206d26a0b8b438b0ea4
Magister <xqjh@mail.ru>
2024-05-25 22:25:54 +0300

    Изменение дизайна поля ввода текста: Закругление краев leadingIcon добавить элемент (закрашивается если введен текст)

--- app/src/main/java/com/composetestingapp/presentation/fragment/ComposeFoo.kt
+++ app/src/main/java/com/composetestingapp/presentation/fragment/ComposeFoo.kt
@@ -1,18 +1,17 @@
 package com.composetestingapp.presentation.fragment
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,38 +20,57 @@ import kotlinx.coroutines.flow.MutableStateFlow
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun UserInputText(textLine: MutableStateFlow<String>) {
+fun UserInputText(
+    textLine: MutableStateFlow<String>,
+    modifier: Modifier = Modifier,
+    addFun: (String) -> Unit
+) {
     val text = textLine.collectAsState()
 
     TextField(
         value = text.value, onValueChange = {
             textLine.value = it
         },
-        colors = TextFieldDefaults.textFieldColors(textColor = Color.Magenta)
+        modifier = modifier.padding(horizontal = 16.dp, vertical = 4.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.Magenta,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        shape = RoundedCornerShape(16.dp),
+        leadingIcon = {
+            IconForInput(
+                isEnabled = text.value.isNotEmpty(),
+                clickFun = { addFun.invoke(text.value) })
+        }
+
     )
 }
+
+@Composable
+fun IconForInput(isEnabled: Boolean, clickFun: () -> Unit) {
+    val tintColor = if (isEnabled) Color.Magenta else Color.LightGray
+    Icon(
+        modifier = Modifier.clickable { clickFun.invoke() },
+        imageVector = Icons.Default.AddCircle,
+        contentDescription = null,
+        tint = tintColor
+    )
+
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 @Preview
 fun UiTextFieldPreview() {
+    val internalTxt = MutableStateFlow<String>("Input")
     Column {
-        UiUserInput()
-        UiUserInput()
+        UserInputText(internalTxt) {}
+        IconForInput(isEnabled = true) {}
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun UiUserInput() {
-    var txt by remember { mutableStateOf("Text") }
-    Card(
-        colors = CardDefaults.cardColors(containerColor = Color.Gray)
-    ) {
-        TextField(
-            modifier = Modifier.padding(16.dp),
-            value = txt,
-            onValueChange = { txt = it }
-        )
-    }
-}
+
+
+
--- app/src/main/java/com/composetestingapp/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/com/composetestingapp/presentation/fragment/SettingsFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -22,7 +24,9 @@ class SettingsFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 Column {
-                    UserInputText(viewModel.inputText)
+                    UserInputText(viewModel.inputText, modifier = Modifier.fillMaxWidth()){
+
+                    }
                 }
             }
         }
